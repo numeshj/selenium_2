f7f72856ccaa18ac2f39268a868899d1acfe55ed
numeshj <numeshjayamanne@gmail.com>
2026-02-23 18:55:09 +0530

    Practice

--- src/test/java/TextBoxExample.java
+++ src/test/java/TextBoxExample.java
@@ -1,4 +1,7 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -17,33 +20,32 @@ public class TextBoxExample {
     @Test
     public  void testTextBox() {
         // 01. Type the name
-
+        WebElement typeName = driver.findElement(By.id("j_idt88:name"));
+        typeName.sendKeys("Numesh Jayamanne");
 
         // 02. Append Country to the City
-
-
+        WebElement appendCountry = driver.findElement(By.id("j_idt88:j_idt91"));
+        appendCountry.sendKeys(" India");
 
         // 03. Verify if text box is disabled
-
-
+        WebElement isDisabled = driver.findElement(By.id("j_idt88:j_idt93"));
+        if (isDisabled.isEnabled()) {
+            System.out.println("The box is enabled");
+        } else {
+        System.out.println("The box is disabled"); }
 
         // 04. Clear the typed text.
-
-
-
-
-
+        WebElement clearText = driver.findElement(By.id("j_idt88:j_idt95"));
+        clearText.clear();
 
         // 05. Retrieve the typed text.
-
-
-
+        WebElement retrieveText = driver.findElement(By.id("j_idt88:j_idt97"));
+        String text = retrieveText.getAttribute("value");
+        System.out.println("The typed text is: " + text);
 
         // 06. Type email and Tab. Confirm control moved to next element.
-
-
-
-
+        WebElement typeEmail = driver.findElement(By.id("j_idt88:j_idt99"));
+        typeEmail.sendKeys("numesh@gmail.com" + Keys.TAB + "Confirmed control moved to next element.");
 
         // 07. Type about yourself
 
